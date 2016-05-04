3b0e9839bfb03cb17b2fc749e87232e9b4b855d8
splitice <mat999@gmail.com>
2016-05-04 14:37:49 +1000

    Initial code commit

---
+++ Rage4Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Heijden.DNS;
+
+namespace Rage4Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nameserver = "ns1.r4ns.com";
+            String domainFormat = args[0];
+            int testCount = 10000;
+
+            Resolver r = new Resolver(Dns.GetHostAddresses(nameserver)[0], 53);
+            for (int i = 0; i < testCount; i++)
+            {
+                Console.WriteLine("Performing request {0}", i);
+                r.GetHostByName(String.Format(domainFormat, i));
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
